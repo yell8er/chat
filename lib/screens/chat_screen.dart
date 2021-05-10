24177aa37e563d956d8e12e22aa8a232616fc932
atwork <yell4510@gmail.com>
2021-05-10 11:30:15 +0300

    init

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/hcpYr2K9nkOKtm7EWW6n/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
+      ),
+    );
+  }
+}
