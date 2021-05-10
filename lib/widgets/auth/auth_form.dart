e2f97d0aa3da10da3eeb3c01d1acfc235fc5cbe5
atwork <yell4510@gmail.com>
2021-05-10 19:28:09 +0300

    working with image picker

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:fc6/widgets/pickers/user_image_picker.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -55,6 +56,7 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  UserImagePicker(),
                   TextFormField(
                     key: ValueKey('email'),
                     validator: (value) {
---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    void _pickImage() async {
+      final pickedImageFile =
+          await ImagePicker.pickImage(source: ImageSource.camera);
+      setState(() {
+        _pickedImage = pickedImageFile;
+      });
+    }
+
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add image'),
+        ),
+      ],
+    );
+  }
+}
